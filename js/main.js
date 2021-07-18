08bbe893e8c4aae535626573f04f9c8407914e71
Joesh Gichon <joeshgichon56@gmail.com>
2021-07-19 01:16:28 +0300

    This will add a day and month date validation in js file

--- js/main.js
+++ js/main.js
@@ -9,14 +9,25 @@ const submitData = document.getElementById("submit");
 submitData.addEventListener("click", function(e){
     e.preventDefault();
     var month = inputMonth.value;
-    var day = inputMonth.value;
-    var year = inputMonth.value;
+    var day = inputDay.value;
+    var year = inputYear.value;
+
+    console.log(day);
+    console.log(month);
+    console.log(year);
+
+    if(month<0 && month>12 && day<0 && day>31){
+        alert("Invalid month");
+    };
 
     var radioCh = document.getElementsByName("gender").forEach(radio =>{
         if (radio.checked){
             console.log(radio.value)
         }
-    })
+    });
+
+        
+
 
 });
 
